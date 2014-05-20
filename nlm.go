f9bd7950f20c3c17790691d7ce2cdb48c3baf533
glen newton <glen.newton@gmail.com>
2014-05-20 12:46:29 -0400

    Moved NLM info to separate file (nlm.go)

---
+++ nlm.go
@@ -0,0 +1,6 @@
+package main
+
+const NLM_TERMS_URL="https://www.nlm.nih.gov/mesh/termscon.html"
+const MESH_OWNER="U.S. National Library of Medicine is the creator, maintainer, and provider of this data"
+const MESH_VERSION="2014 MeSH"
+const MESH_COPYRIGHT="Copyright U.S. National Library of Medicine"
